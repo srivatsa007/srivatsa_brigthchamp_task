784cc610e85da4fcf77d3e6e7447868e3fc13ffe
Srivats <srivatsa99damaraju@gmail.com>
2024-03-16 16:26:08 +0530

    created app, saved assets

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+
+  const cards = [
+
+  ]
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
